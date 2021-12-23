acbe29ee855bf23781e24baac9d88fe3a84dad7b
Your Name <angelgonzalezacevedo12@outlook.com>
2021-12-22 23:22:38 -0500

    vf 1.1.9

--- database.js
+++ database.js
@@ -11,13 +11,8 @@ const config_1 = __importDefault(require("./config"));
         mongoose_1.default.set('useNewUrlParser', true);
         mongoose_1.default.set('useFindAndModify', false);
         mongoose_1.default.set('useCreateIndex', true);
-        //servidor en ubuntu prod
-        //const respserver=await mongoose.connect("mongodb://root:admin123@144.126.133.247:27017/appstock?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
-        //mongodbatlas 
-        // const reponse=await mongoose.connect(`mongodb+srv://admin:admin123@appstock.xcrnk.mongodb.net/appstock?retryWrites=true&w=majority`); 
-        const response = await mongoose_1.default.connect(`mongodb://${config_1.default.MONGO_USER}:${config_1.default.MONGO_PASSWORD}@${config_1.default.MONGO_HOST}/${config_1.default.MONGO_DB}?authSource=admin`);
-        // const reponse=await mongoose.connect(`mongodb+srv://admin:${config.MONGO_PASSWORD}@appstock.xcrnk.mongodb.net/${config.MONGO_DB}?retryWrites=true&w=majority`);  
-    }
+       const response = await mongoose_1.default.connect(`mongodb://${config_1.default.MONGO_USER}:${config_1.default.MONGO_PASSWORD}@${config_1.default.MONGO_HOST}/${config_1.default.MONGO_DB}?authSource=admin`);
+   }
     catch (error) {
         console.error(error);
     }
